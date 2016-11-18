2368f76ea1115eb132e0efa8fea61a34bb0776b1
Dago86 <diego.deangelis@studio.unibo.it>
2016-11-18 15:20:31 +0100

    Create TCPServer.c

---
+++ TCPServer.c
@@ -0,0 +1,60 @@
+ char buf[MAXLINE];
+ struct sockaddr_in cliaddr, servaddr;
+
+ //Create a socket for the soclet
+ //If sockfd<0 there was an error in the creation of the socket
+ if ((listenfd = socket (AF_INET, SOCK_STREAM, 0)) <0) {
+  perror("Problem in creating the socket");
+  exit(2);
+ }
+
+
+ //preparation of the socket address
+ servaddr.sin_family = AF_INET;
+ servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+ servaddr.sin_port = htons(SERV_PORT);
+
+ //bind the socket
+ bind (listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+
+ //listen to the socket by creating a connection queue, then wait for clients
+ listen (listenfd, LISTENQ);
+
+ printf("%s\n","Server running...waiting for connections.");
+
+ for ( ; ; ) {
+
+  clilen = sizeof(cliaddr);
+  //accept a connection
+  connfd = accept (listenfd, (struct sockaddr *) &cliaddr, &clilen);
+
+  printf("%s\n","Received request...");
+
+  if ( (childpid = fork ()) == 0 ) {//if it’s 0, it’s child process
+
+  printf ("%s\n","Child created for dealing with client requests");
+
+  //close listening socket
+  close (listenfd);
+
+  while ( (n = recv(connfd, buf, MAXLINE,0)) > 0)  {
+
+   char *str= "Inserisci il nome dell'oggetto che vuoi muovere";
+	
+   send(connfd, str, n, 0);
+   puts(buf);
+   
+
+   //printf("%s","String received from and resent to the client:");
+   //puts(buf);
+   //send(connfd, buf, n, 0);
+  }
+
+  if (n < 0)
+   printf("%s\n", "Read error");
+  exit(0);
+ }
+ //close socket of the server
+ close(connfd);
+}
+}
