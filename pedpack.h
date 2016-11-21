d2f458661f38977867101f533810b30ec1f3915b
rederik92 <m.erik92@hotmail.it>
2016-11-21 17:09:00 +0100

    Add files via upload

---
+++ pedpack.h
@@ -0,0 +1,22 @@
+#ifndef PEDPACK_H_
+#DEFINE PEDPACK_H_
+
+// Il pedone vero e proprio
+typedef struct pedina{
+	int x;
+	int y;
+} ped;
+
+//Pacchetto da inviare al server
+typedef struct pacchetto{
+	char dir;
+	int passi;
+} pac;
+
+//inserisce d e p nei rispettivi campi del pacchetto
+void riempi_pacchetto(struct pacchetto* pa, char d, int p);
+
+//muove il pedone
+void movimento(struct pacchetto* pa, struct pedina* pe);
+
+#endif
